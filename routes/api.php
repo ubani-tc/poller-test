f35b8d3ec1afb6defd4e0b695efdf9914ec64574
Ubani Tochukwu <ubani@fincra.com>
2025-02-19 20:46:35 +0100

    second commit

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,14 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/integration/gateway', [IntegrationGatewayController::class, 'gateway']);
+Route::post('/webhooks/wema/callback', function (Request $request){
+    return $request->all();
+});
+
+Route::post('/webhooks/globus/callback', function (Request $request){
+    return $request->all();
+});
+
+Route::post('/webhooks/charges/bank-transfer', function (Request $request){
+    return $request->all();
+});
